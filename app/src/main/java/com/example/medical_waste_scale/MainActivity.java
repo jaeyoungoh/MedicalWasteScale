0d9816d5587c4fd9979a9fc06bbbc2d832579977
오재영 <wodud.oh@gmail.com>
2022-06-16 11:01:06 +0900

    - UI ID 명칭 정리 (id에 right, left 키워드 삭제) - 폰트 사이즈 조정 - HY헤드라인M 폰트 추가 및 반영

--- app/src/main/java/com/example/medical_waste_scale/MainActivity.java
+++ app/src/main/java/com/example/medical_waste_scale/MainActivity.java
@@ -1,41 +1,17 @@
 package com.example.medical_waste_scale;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbDeviceConnection;
-import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
-import android.text.Spannable;
-import android.text.SpannableStringBuilder;
-import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.widget.ToggleButton;
 
-import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.hoho.android.usbserial.driver.UsbSerialPort;
-import com.hoho.android.usbserial.driver.UsbSerialProber;
 import com.hoho.android.usbserial.util.SerialInputOutputManager;
-//import usbserial.util.HexDump;
-
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.EnumSet;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -70,6 +46,5 @@ public class MainActivity extends AppCompatActivity {
         //화면을 켜진 상태로 유지
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-        View receiveBtn = findViewById(R.id.receive_btn);
     }
 }

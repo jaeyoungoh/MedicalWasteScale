e34ce501c79b3bd00bb088c49638bdbff5adce63
오재영 <wodud.oh@gmail.com>
2022-06-24 04:59:58 +0900

    - 시리얼연동 및 테스트 코드 적용

--- app/src/main/java/kr/co/rubeesys/medicalWasteScale/MainActivity.java
+++ app/src/main/java/kr/co/rubeesys/medicalWasteScale/MainActivity.java
@@ -1,23 +1,54 @@
 package kr.co.rubeesys.medicalWasteScale;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.ref.WeakReference;
+import java.util.Set;
 
 import kr.co.rubeesys.medicalWasteScale.databinding.MainBinding;
 import kr.co.rubeesys.medicalWasteScale.databinding.MainLeftContentsBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MainBinding viewBinding;
+    private MainBinding mainActivityBinding;
     private MainLeftContentsBinding leftContentsBinding;
+
+    private UsbService usbService;
+    private TextView nowWeightNumber;
+    private MyHandler mHandler;
+    private final ServiceConnection usbConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName arg0, IBinder arg1) {
+            usbService = ((UsbService.UsbBinder) arg1).getService();
+            usbService.setHandler(mHandler);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            usbService = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        mainActivityBinding = MainBinding.inflate(getLayoutInflater());
+        setContentView(mainActivityBinding.getRoot());
 
         //네비게이션 바(소프트키) 숨김
         final View decorView = getWindow().getDecorView();
@@ -25,13 +56,109 @@ public class MainActivity extends AppCompatActivity {
         //화면을 켜진 상태로 유지
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        mHandler = new MyHandler(this);
+
         leftContentsBinding = MainLeftContentsBinding.inflate(getLayoutInflater());
-        updateWeightNumber(leftContentsBinding, "3.2");
+        mainActivityBinding.imageLogo.setOnClickListener(v -> sendingSerialTestData());
 
     }
 
-    private void updateWeightNumber(MainLeftContentsBinding viewBinding, String num)
-    {
-        viewBinding.nowWeightNumber.setText(num);
+    private void sendingSerialTestData(){
+        final String data = "5.3";
+        if(usbService != null)
+            usbService.write(data.getBytes());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setFilters();  // Start listening notifications from UsbService
+        startService(UsbService.class, usbConnection, null); // Start UsbService(if it was not started before) and Bind it
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(mUsbReceiver);
+        unbindService(usbConnection);
+    }
+
+    private void startService(Class<?> service, ServiceConnection serviceConnection, Bundle extras) {
+        if (!UsbService.SERVICE_CONNECTED) {
+            Intent startService = new Intent(this, service);
+            if (extras != null && !extras.isEmpty()) {
+                Set<String> keys = extras.keySet();
+                for (String key : keys) {
+                    String extra = extras.getString(key);
+                    startService.putExtra(key, extra);
+                }
+            }
+            startService(startService);
+        }
+        Intent bindingIntent = new Intent(this, service);
+        bindService(bindingIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    /*
+     * Notifications from UsbService will be received here.
+     */
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()) {
+                case UsbService.ACTION_USB_PERMISSION_GRANTED: // USB PERMISSION GRANTED
+                    Toast.makeText(context, "USB 준비됨", Toast.LENGTH_LONG).show();
+                    break;
+                case UsbService.ACTION_USB_PERMISSION_NOT_GRANTED: // USB PERMISSION NOT GRANTED
+                    Toast.makeText(context, "USB 권한 획득 실패", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_NO_USB: // NO USB CONNECTED
+                    Toast.makeText(context, "USB 케이블 연결상태를 확인해주세요.", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_DISCONNECTED: // USB DISCONNECTED
+                    Toast.makeText(context, "USB 연결 끊김", Toast.LENGTH_SHORT).show();
+                    break;
+                case UsbService.ACTION_USB_NOT_SUPPORTED: // USB NOT SUPPORTED
+                    Toast.makeText(context, "지원되지 않는 USB 케이블 입니다.", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
+    private void setFilters() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_GRANTED);
+        filter.addAction(UsbService.ACTION_NO_USB);
+        filter.addAction(UsbService.ACTION_USB_DISCONNECTED);
+        filter.addAction(UsbService.ACTION_USB_NOT_SUPPORTED);
+        filter.addAction(UsbService.ACTION_USB_PERMISSION_NOT_GRANTED);
+        registerReceiver(mUsbReceiver, filter);
+    }
+
+    /*
+     * This handler will be passed to UsbService. Data received from serial port is displayed through this handler
+     */
+    private static class MyHandler extends Handler {
+        private final WeakReference<MainActivity> mActivity;
+
+        public MyHandler(MainActivity activity) {
+            mActivity = new WeakReference<>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case UsbService.MESSAGE_FROM_SERIAL_PORT:
+                    String data = (String) msg.obj;
+                    mActivity.get().mainActivityBinding.leftContents.nowWeightNumber.setText(data); //시리얼 데이터 연동
+                    break;
+                case UsbService.CTS_CHANGE:
+                    Toast.makeText(mActivity.get(), "CTS 변화 감지됨",Toast.LENGTH_LONG).show();
+                    break;
+                case UsbService.DSR_CHANGE:
+                    Toast.makeText(mActivity.get(), "DSR 변화 감지됨",Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
     }
 }
